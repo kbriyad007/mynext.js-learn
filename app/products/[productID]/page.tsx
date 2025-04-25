6a94b5bf1060be29433f37351a218348ee1fdc99
kbriyad007 <kb.riyad007@gmail.com>
2025-04-25 23:06:56 +0600

    page.tsx

--- app/products/[productID]/page.tsx
+++ app/products/[productID]/page.tsx
@@ -1,8 +1,11 @@
-type Props = {
+// app/products/[productID]/page.tsx
+
+interface ProductPageProps {
   params: {
     productID: string;
   };
-};
-export default function ProductDetail({params}:Props) {
-  return <h1>Product details{params.productID}</h1>;
+}
+
+export default function ProductDetail({ params }: ProductPageProps) {
+  return <h1>Product details: {params.productID}</h1>;
 }
