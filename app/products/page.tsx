eb568b2ba469ce14989edd7c05a1fc7f3d47a3bf
kbriyad007 <kb.riyad007@gmail.com>
2025-04-25 22:01:20 +0600

    page.tsx

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,8 +1,10 @@
 export default function product(){
   return (
+    <>
     <h1>Product List</h1>
     <h2>Product List 1</h2>
-    <h2>Product List 1</h2>
-    <h2>Product List 1</h2>
+    <h2>Product List 2</h2>
+    <h2>Product List 3</h2>
+    </>
     );
 }
